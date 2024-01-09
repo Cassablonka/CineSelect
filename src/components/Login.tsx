0042dd1ba322c22ecac33da96e357e2a6fd5581f
Cassablonka <cassapimpalapure@gmail.com>
2024-01-09 21:08:30 +0530

    added new languages

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -6,10 +6,14 @@ import { useTranslation } from 'react-i18next';
 const Login = () => {
   const [isSignedIn, setIsSignedIn] = useState(true)
   const [errMessage, setErrMessage] = useState('')
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
 
   const handleSignUp = () => {
     setIsSignedIn(!isSignedIn)
+  }
+
+  const handleLanguageChange = (e: any) => {
+    i18n.changeLanguage(e.target.value)
   }
 
   let email = useRef<HTMLInputElement>(null)
@@ -26,15 +30,31 @@ const Login = () => {
     <>
       <div className="absolute">
         <img
-          className=""
+          className="bg-gradient-to-b from-black"
           src="https://assets.nflxext.com/ffe/siteui/vlv3/c38a2d52-138e-48a3-ab68-36787ece46b3/eeb03fc9-99c6-438e-824d-32917ce55783/IN-en-20240101-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
           alt=""
         />
       </div>
+      <div className='flex'>
       <div className="absolute bg-gradient-to-b from-black">
         <h1 className="p-7 opacity-100 text-red-500 text-4xl font-bold font-sans subpixel-antialiased tracking-wider">
           CINESELECT
         </h1>
+      </div>
+        <div className='absolute right-0 py-6 px-20  text-white bg-gradient-to-b from-black'>
+          <div className='bg-red-700 rounded-md flex'>
+          <span className='font-bold pl-2.5 pr-1 py-2'>⎈</span>
+          <div className='px-2'>
+          <select className='py-1 pr-1.5 my-1.5 rounded-sm bg-red-700 font-semibold outline-none' onChange={(e) => handleLanguageChange(e)}>
+          <option value="en">English</option>
+          <option value="hi">हिंदी</option>
+          <option value="mr">मराठी</option>
+          <option value="kn">ಕನ್ನಡ</option>
+        </select>
+          </div>
+
+          </div>
+        </div>
       </div>
       <div className="absolute w-1/4 mx-auto left-0 right-0 my-32 bg-black py-8 px-12 rounded-md bg-opacity-80">
         <form action="">
---
+++ src/utils/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCG5W1B3YXe17LEWVw1gxts9Sn11Reg5s8",
+  authDomain: "cineselect-18c5d.firebaseapp.com",
+  projectId: "cineselect-18c5d",
+  storageBucket: "cineselect-18c5d.appspot.com",
+  messagingSenderId: "92202205094",
+  appId: "1:92202205094:web:67d768ffbe9213cfea9967",
+  measurementId: "G-G0BR1BGPJ6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
