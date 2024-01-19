d6143eda322fea7375343438549c69eb81fdab46
Cassablonka <cassapimpalapure@gmail.com>
2024-01-19 12:05:10 +0530

    tree shaking the code

--- src/components/Browse.tsx
+++ src/components/Browse.tsx
@@ -2,17 +2,13 @@ import Header from './Header'
 import MainContainer from './MainContainer'
 import SecondaryContainer from './SecondaryContainer'
 import { useNowPlayingMovies } from '../hooks/useNowPlayingMovies'
-import { usePopularMovies } from '../hooks/usePopularMovies'
-import { useFutureReleaseMovies } from '../hooks/useFutureReleaseMovies'
-import { useTopRatedMovies } from '../hooks/useTopRatedMovies'
 import { useSelector } from 'react-redux'
 import GptSearchPage from './GptSearchPage'
 
 const Browse = () => {
+
   useNowPlayingMovies()
-  usePopularMovies()
-  useFutureReleaseMovies()
-  useTopRatedMovies()
+  
   const searchFlag = useSelector((store: any) => store?.gpt?.gptSearch)
   return (
     <>
